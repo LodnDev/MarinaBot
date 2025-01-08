12ea97508470751b24429d0685eb7f044ae8ca57
LodnDev <lucasoliveiradonascimento8@gmail.com>
2025-01-07 23:43:04 -0400

    Avançando no comando Iniciar

--- comandos/iniciar.py
+++ comandos/iniciar.py
@@ -3,16 +3,31 @@ from discord import app_commands
 from discord.ext import commands
 
 class iniciar(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
         super().__init__()
 
     @app_commands.command()
     async def iniciar(self, interaction:discord.Interaction):
-        embed = discord.Embed(title='O Incio', description=f'OK {interaction.user.display_name}, Vamos Começar a nossa Incrivel Jornada')
-        embed.color = discord.Color.gold()
+        async def comprar_casa(interactio:discord.Interaction):
+            embedCasa = discord.Embed(title='Passo 1', 
+                                      description='Primeiro passo para nossa jornada se iniciar é construir a sua casa')
+            embed.color = discord.Color.green()
+            embed.set_footer(text='Escolha qual tipo de casa Você vai comprar')
 
-        await interaction.response.send_message(embed=embed)
+            await interactio.response.edit_message(embed=embedCasa)
+
+        embed = discord.Embed(title='O Incio', description=f'OK {interaction.user.display_name}, Vamos Começar!')
+        embed.color = discord.Color.gold()
+        embed.set_footer(text='Clique no Botão para Continuar')
+
+        view = discord.ui.View()
+        botao = discord.ui.Button(label='Iniciar')
+        botao.callback = comprar_casa
+
+        view.add_item(botao)
+
+        await interaction.response.send_message(embed=embed, view=view)
 
 async def setup(bot):
     await bot.add_cog(iniciar(bot))
